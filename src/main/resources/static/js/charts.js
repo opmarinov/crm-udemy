edc5db29d9634c507e6fd0d2d45b300740858563
plamen.marinov <plamen.marinov@meditcon.com>
2018-09-05 17:56:21 +0300

    Learned and added how to use layouts in thymeleaf. Added a third chart.

--- src/main/resources/static/js/charts.js
+++ src/main/resources/static/js/charts.js
@@ -1,17 +1,39 @@
-window.onload = function () {
-  var labels = {};
-  var y = {};
+var data;
 
+window.onload = function () {
+
+  /// Make a call to the rest end point to retrieve the data for the charts.
   $.ajax({
     method: "GET",
     url: "http://localhost:8086/retrieve/contribution"
   })
   .done(function (res) {
     console.log(res);
+    data = res;
 
-    populatePieChart(res);
-    populateBarChart(res);
+    ///Initialize with the pie chart.
+    populatePieChart(res)
   });
+
+
+  $('#selectChart')
+  .change(function () {
+    var str = "";
+    $( "select option:selected" ).each(function() {
+      str += $( this ).val();
+    });
+
+    if(str === 'Pie')
+      populatePieChart(data);
+    if(str === 'Bar')
+      populateBarChart(data);
+    if(str === 'Line')
+      populateLineChart(data);
+
+    console.log('Hey i changed! ', str);
+  });
+
+
 };
 
 function populatePieChart(result) {
@@ -39,20 +61,46 @@ function populateBarChart(result) {
   var options = {
     animationEnabled: true,
     title: {
-      text: "GDP Growth Rate - 2016"
+      text: "Contribution Chart"
     },
     axisY: {
-      title: "Growth Rate (in %)",
-      suffix: "%",
-      includeZero: false
+      title: "All in all",
+      includeZero: true
     },
     axisX: {
-      title: "Countries"
+      title: "Employees"
     },
     data: [{
       type: "column",
       dataPoints: result
     }]
   };
-  $("#chartContainer2").CanvasJSChart(options);
+  $("#chartContainer").CanvasJSChart(options);
+}
+
+/*$('select').change(function() {
+  console.log('Hey i changed!');
+  var val = $('secelct').val();
+
+  if(val === 'Pie')
+    populatePieChart(data);
+  if(val === 'Bar')
+    populateBarChart(data);
+});*/
+
+function populateLineChart(result){
+  var options = {
+    animationEnabled: true,
+    title:{
+      text: "Contribution"
+    },
+    axisY: {
+      includeZero: true
+    },
+    data: [{
+      type: "spline",
+      dataPoints: result
+    }]
+  };
+  $("#chartContainer").CanvasJSChart(options);
 }
